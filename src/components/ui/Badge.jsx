e692f9307b2e25f015a95f2dedb94efe63ae04ae
Quvonch299 <muminovquvonchbek85@gmail.com>
2026-05-12 00:15:45 +0500

    endsa

--- src/components/ui/Badge.jsx
+++ src/components/ui/Badge.jsx
@@ -10,7 +10,7 @@ export default function Badge({ link, text, link2, text2 }) {
             <Link to={link} target="_blank" rel="noopener noreferrer">
                 {text}
             </Link>
-            /
+            
             {
                 link2 &&
                 <Link to={link2} target="_blank" rel="noopener noreferrer">
