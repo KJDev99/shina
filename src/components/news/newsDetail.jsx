ce9067131d789d4d5f8a2aa62d0397776327fd2f
abdunabiyev-o1o <abdunabiyevsiroj32@gmail.com>
2026-05-13 16:18:34 +0500

    tayyor boldi

--- src/components/news/newsDetail.jsx
+++ src/components/news/newsDetail.jsx
@@ -4,6 +4,11 @@ import Badge from "../ui/Badge";
 export default function NewsDetail() {
   return (
     <div>
+      <div className="flex items-center gap-2 mb-6 text-[14px]">
+        <span className="text-black">Главная</span>
+        <span className="text-[#999999] text-[10px]">❯</span>
+        <span className="text-[#999999]">Caterpillar</span>
+      </div>
     </div>
   );
 }
--- src/pages/siroj/contact.jsx
+++ src/pages/siroj/contact.jsx
@@ -1,5 +1,6 @@
 import { ArrowUpRight } from "lucide-react";
 import FormPost from "../../components/ui/FormPost";
+import CantactForm from "../../components/ui/cantactform";
 
 export default function Contact() {
   return (
@@ -20,10 +21,10 @@ export default function Contact() {
         <div className="flex gap-[11px] ">
           <div className="w-[471px] h-[262px] bg-white rounded-[30px] p-8 flex flex-col justify-between">
             <div className="flex justify-between items-start">
-              <span className="text-[#999999] text-[16px]">Наш адрес</span>
+              <span className="text-[#999999] text-lg font-medium ">Наш адрес</span>
               <ArrowUpRight className="text-black w-6 h-6" />
             </div>
-            <div className="text-[24px] font-semibold leading-tight text-black max-w-[300px]">
+            <div className="text-[24px] font-semibold leading-tight text-black max-w-[319px]">
               г. Санкт-Петербург <br />
               Невский проспект, д. 30, <br />
               офисы 5.4 и 6.5
@@ -32,7 +33,7 @@ export default function Contact() {
 
           <div className="w-[471px] h-[262px] bg-white rounded-[30px] p-8 flex flex-col justify-between">
             <div className="flex justify-between items-start">
-              <span className="text-[#999999] text-[16px]">
+              <span className="text-[#999999] text-lg font-medium">
                 Контактные телефоны
               </span>
               <ArrowUpRight className="text-black w-6 h-6" />
@@ -40,10 +41,10 @@ export default function Contact() {
             <div className="space-y-6">
               <div className="flex items-center justify-between">
                 <div>
-                  <p className="text-[#999999] text-[12px] mb-1">
+                  <p className="text-[#999999] text-sm mb-1 font-normal ">
                     Запчасти для спецтехники:
                   </p>
-                  <p className="text-[22px] font-bold text-black">
+                  <p className="text-2xl font-semibold text-black">
                     +7 (921) 905-70-21
                   </p>
                 </div>
@@ -65,10 +66,10 @@ export default function Contact() {
               </div>
               <div className="flex items-center justify-between">
                 <div>
-                  <p className="text-[#999999] text-[12px] mb-1">
+                  <p className="text-[#999999] text-sm mb-1 font-normal">
                     Шины для спецтехники:
                   </p>
-                  <p className="text-[22px] font-bold text-black">
+                  <p className="text-2xl font-semibold text-black">
                     +7 (921) 306-51-25
                   </p>
                 </div>
@@ -93,28 +94,28 @@ export default function Contact() {
 
           <div className="w-[471px] h-[262px] bg-white rounded-[30px] p-8 flex flex-col justify-between">
             <div className="flex justify-between items-start">
-              <span className="text-[#999999] text-[16px]">
+              <span className="text-[#999999] text-lg font-medium">
                 Электронная почта
               </span>
               <ArrowUpRight className="text-black w-6 h-6" />
             </div>
             <div className="space-y-6">
               <div>
-                <p className="text-[#999999] text-[12px] mb-1">Запчасти:</p>
-                <p className="text-[22px] font-bold text-black underline underline-offset-4">
+                <p className="text-[#999999] text-sm font-normal mb-1">Запчасти:</p>
+                <p className="text-2xl font-semibold text-black underline underline-offset-4">
                   info@maksan-group.ru
                 </p>
               </div>
               <div>
-                <p className="text-[#999999] text-[12px] mb-1">Шины:</p>
-                <p className="text-[22px] font-bold text-black underline underline-offset-4">
+                <p className="text-[#999999] text-sm mb-1 font-normal">Шины:</p>
+                <p className="text-2xl font-semibold text-black underline underline-offset-4">
                   andrey.ivanov@maksan-group.ru
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <FormPost />
+<CantactForm/>  
       </div>
     </div>
   );
